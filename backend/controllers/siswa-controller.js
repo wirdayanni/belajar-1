97787b530d36dd29840410b329881518c3d2296c
wirdayanni <wirdayanni@gmail.com>
2025-10-05 01:02:51 +0700

    update file controller dan route

--- backend/controllers/siswa-controller.js
+++ backend/controllers/siswa-controller.js
@@ -1,5 +1,76 @@
-export const getAllSiswa = async (req, res) => {};
-export const getSiswaById = async (req, res) => {};
-export const createSiswa = async (req, res) => {};
-export const updateSiswa = async (req, res) => {};
-export const deleteSiswa = async (req, res) => {};
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();  
+
+// Get Semua Siswa
+export const getAllSiswa = async (req, res) => {
+    try {
+        const response = await prisma.siswa.findMany()
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+};
+
+// Get Siswa By Id
+export const getSiswaById = async (req, res) => {
+    try {
+        const response = await prisma.siswa.findUnique({
+            where:{
+                id : Number(req.params.id)
+            }
+        })
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+};
+
+// Create Siswa
+export const createSiswa = async (req, res) => {
+    const {nama, umur} = req.body
+    try {
+        const response = await prisma.siswa.create({
+            data: {
+                nama,
+                umur
+            }
+        })
+        res.status(201).json(response)
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+};
+
+// Update Siswa
+export const updateSiswa = async (req, res) => {
+    const {nama, umur} = req.body
+    try {
+        const response = await prisma.siswa.update({
+            where:{
+                id : Number(req.params.id)
+            },
+            data: {
+                nama,
+                umur
+            }
+        })
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+};
+
+// Delete Siswa
+export const deleteSiswa = async (req, res) => {
+    try {
+        const response = await prisma.siswa.delete({
+            where:{
+                id: Number(req.params.id)
+            }
+        })
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import siswaRoute from"./routes/siswa-route.js"
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json())
+app.use(siswaRoute);
 
 app.get("/", (_, res) => res.send("Backend is running..."));
 
