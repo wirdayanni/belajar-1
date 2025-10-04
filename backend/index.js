3ac22043f619b737b3a46c7448d07a6caa4eefdf
wirdayanni <wirdayanni@gmail.com>
2025-10-05 00:03:33 +0700

    setup awal backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.get("/", (_, res) => res.send("Backend is running..."));
+
+const PORT = 8000;
+app.listen(PORT, () =>
+  console.log(`Backend server is running on http://localhost:${PORT}`)
+);
