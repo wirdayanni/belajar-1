1517fc6d526f957ce58f68964e5a600302fa0e8f
wirdayanni <wirdayanni@gmail.com>
2025-10-05 00:08:37 +0700

    add file siswa-controller.js

---
+++ backend/controllers/siswa-controller.js
@@ -0,0 +1,5 @@
+export const getAllSiswa = async (req, res) => {};
+export const getSiswaById = async (req, res) => {};
+export const createSiswa = async (req, res) => {};
+export const updateSiswa = async (req, res) => {};
+export const deleteSiswa = async (req, res) => {};
